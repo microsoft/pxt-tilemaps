ded8dd8d4da206425d9bfad6fb98d3ebc85246cd
Richard Knoll <riknoll@users.noreply.github.com>
2019-12-23 10:58:40 -0800

    Cleaning up block definitions and adding groups

--- overworld.ts
+++ overworld.ts
@@ -10,6 +10,7 @@ enum WorldDirection {
 }
 
 //% color=#b8849b
+//% groups='["Creation", "Connections", "World Grid"]'
 namespace overworld {
     export const OVERWORLD_MAP_ID = 7686; 
     export const MAP_LOADED_EVENT = 7687;
@@ -79,27 +80,14 @@ namespace overworld {
     }
 
     /**
-     * Executes a piece of code when an overworld tilemap is loaded.
-     */
-    //% block="on map loaded $map"
-    export function onMapLoaded(cb: (map: WorldMap) => void) {
-        control.onEvent(OVERWORLD_MAP_ID, MAP_LOADED_EVENT, function () {
-            cb(getLoadedMap());
-        });
-    }
-
-    /**
-     * Executes a piece of code when an overworld tilemap is unloaded.
-     */
-    //% block="on map unloaded $map"
-    export function onMapUnloaded(cb: (map: WorldMap) => void) {
-        OverWorldState.getInstance().addUnloadListener(cb);
-    }
-
-    /**
      * Creates an overworld tilemap.
      */
+    //% blockId=create_overworld_map
     //% block="create map $tilemap"
+    //% tilemap.fieldEditor="tilemap"
+    //% tilemap.fieldOptions.decompileArgumentAsString="true"
+    //% tilemap.fieldOptions.filter="tile"
+    //% group="Creation" weight=50 blockGap=8
     export function createMap(tilemap: tiles.TileMapData): WorldMap {
         return new WorldMap(tilemap);
     }
@@ -108,6 +96,8 @@ namespace overworld {
      * Loads an overworld tilemap.
      */
     //% block="load map $map"
+    //% map.shadow=create_overworld_map
+    //% group="Creation" weight=40 blockGap=8
     export function loadMap(map: WorldMap) {
         const loaded = getLoadedMap();
 
@@ -127,26 +117,34 @@ namespace overworld {
     }
 
     /**
-     * Loads an overworld tilemap at a given column and row in the
-     * overworld grid.
+     * Returns the loaded overworld map.
      */
-    //% block="load map at world col $worldColumn row $worldRow"
-    export function loadMapAt(worldColumn: number, worldRow: number) {
-        loadMap(getMapAtWorldLocation(worldColumn, worldRow));
-        OverWorldState.getInstance().loadedColumn = worldColumn;
-        OverWorldState.getInstance().loadedRow = worldRow;
+    //% block="loaded map"
+    //% group="Creation" weight=30
+    export function getLoadedMap(): WorldMap {
+        return getMapAtWorldLocation(loadedWorldColumn(), loadedWorldRow());
     }
 
     /**
-     * Loads the overworld tilemap adjacent to the loaded map in the
-     * given direction.
+     * Executes a piece of code when an overworld tilemap is loaded.
      */
-    //% block="load map in direction $direction"
-    export function loadMapInDirection(direction: WorldDirection) {
-        loadMapAt(
-            tilemap.columnInDirection(loadedWorldColumn(), direction),
-            tilemap.rowInDirection(loadedWorldRow(), direction)
-        );
+    //% block="on map loaded $map"
+    //% draggableParameters="reporter"
+    //% group="Creation" weight=20 blockGap=8
+    export function onMapLoaded(cb: (map: WorldMap) => void) {
+        control.onEvent(OVERWORLD_MAP_ID, MAP_LOADED_EVENT, function () {
+            cb(getLoadedMap());
+        });
+    }
+
+    /**
+     * Executes a piece of code when an overworld tilemap is unloaded.
+     */
+    //% block="on map unloaded $map"
+    //% draggableParameters="reporter"
+    //% group="Creation" weight=10 blockGap=8
+    export function onMapUnloaded(cb: (map: WorldMap) => void) {
+        OverWorldState.getInstance().addUnloadListener(cb);
     }
 
     /**
@@ -154,22 +152,37 @@ namespace overworld {
      * given id.
      */
     //% block="load connected map $connectionID"
+    //% group="Connections" weight=30 blockGap=8
     export function loadConnectedMap(connectionID: number) {
         loadMap(getConnectedMap(getLoadedMap(), connectionID));
     }
 
     /**
-     * Returns the loaded overworld map.
+     * Connects the source map to a destination map by the given ID.
      */
-    //% block="loaded map"
-    export function getLoadedMap(): WorldMap {
-        return getMapAtWorldLocation(loadedWorldColumn(), loadedWorldRow());
+    //% block="connect $sourceMap to $destination with ID $connectionID"
+    //% sourceMap.shadow=variables_get
+    //% sourceMap.defl=sourceMap
+    //% destination.shadow=create_overworld_map
+    //% group="Connections" weight=20 blockGap=8
+    export function connectMapById(sourceMap: WorldMap, destination: WorldMap, connectionID: number) {
+        if (!sourceMap) return;
+
+        for (const connection of sourceMap.connections) {
+            if (connection.id === connectionID) {
+                connection.map = destination;
+                return;
+            }
+        }
+        sourceMap.connections.push(new WorldMapConnection(connectionID, destination));
     }
 
     /**
      * Gets the destination map connected to the source map by the given ID.
      */
-    //% block="get map connected to $source by $connectionID"
+    //% block="get map connected to $source by ID $connectionID"
+    //% source.shadow=create_overworld_map
+    //% group="Connections" weight=10 blockGap=8
     export function getConnectedMap(source: WorldMap, connectionID: number): WorldMap {
         if (!source) return null;
 
@@ -181,25 +194,55 @@ namespace overworld {
     }
 
     /**
-     * Connects the source map to a destination map by the given ID.
+     * Set the map at the specified column and row in the overworld grid.
      */
-    //% block="connect $source to $destination by $connectionID"
-    export function connectMapById(source: WorldMap, destination: WorldMap, connectionID: number) {
-        if (!source) return;
-        
-        for (const connection of source.connections) {
-            if (connection.id === connectionID) {
-                connection.map = destination;
-                return;
-            }
-        }
-        source.connections.push(new WorldMapConnection(connectionID, destination));
+    //% block="set map at world col $worldColumn row $worldRow to $map"
+    //% map.shadow=create_overworld_map
+    //% group="World Grid" weight=60 blockGap=8
+    export function setWorldLocationToMap(worldColumn: number, worldRow: number, map: WorldMap) {
+        OverWorldState.getInstance().setMapAtLocation(worldColumn, worldRow, map);
+    }
+
+    /**
+     * Loads an overworld tilemap at a given column and row in the
+     * overworld grid.
+     */
+    //% block="load map at world col $worldColumn row $worldRow"
+    //% group="World Grid" weight=50 blockGap=8
+    export function loadMapAt(worldColumn: number, worldRow: number) {
+        loadMap(getMapAtWorldLocation(worldColumn, worldRow));
+        OverWorldState.getInstance().loadedColumn = worldColumn;
+        OverWorldState.getInstance().loadedRow = worldRow;
+    }
+
+    /**
+     * Loads the overworld tilemap adjacent to the loaded map in the
+     * given direction.
+     */
+    //% block="load map in direction $direction"
+    //% direction.shadow=direction_editor
+    //% group="World Grid" weight=40
+    export function loadMapInDirection(direction: number) {
+        loadMapAt(
+            tilemap.columnInDirection(loadedWorldColumn(), direction),
+            tilemap.rowInDirection(loadedWorldRow(), direction)
+        );
+    }
+
+    /**
+     * Get the map at the specified column and row in the overworld grid.
+     */
+    //% block="get map at world col $worldColumn row $worldRow"
+    //% group="World Grid" weight=30 blockGap=8
+    export function getMapAtWorldLocation(worldColumn: number, worldRow: number): WorldMap {
+        return OverWorldState.getInstance().getMapAtLocation(worldColumn, worldRow);
     }
 
     /**
      * Get the world column of the loaded overworld map.
      */
     //% block="loaded world column"
+    //% group="World Grid" weight=20 blockGap=8
     export function loadedWorldColumn() {
         return OverWorldState.getInstance().loadedColumn;
     }
@@ -208,23 +251,8 @@ namespace overworld {
      * Get the world row of the loaded overworld map.
      */
     //% block="loaded world row"
+    //% group="World Grid" weight=10 blockGap=8
     export function loadedWorldRow() {
         return OverWorldState.getInstance().loadedRow;
-    }
-
-    /**
-     * Get the map at the specified column and row in the overworld grid.
-     */
-    //% block="get map at world col $worldColumn row $worldRow"
-    export function getMapAtWorldLocation(worldColumn: number, worldRow: number): WorldMap {
-        return OverWorldState.getInstance().getMapAtLocation(worldColumn, worldRow);
-    }
-
-    /**
-     * Set the map at the specified column and row in the overworld grid.
-     */
-    //% block="set map at world col $worldColumn row $worldRow to $map"
-    export function setWorldLocationToMap(worldColumn: number, worldRow: number, map: WorldMap) {
-        OverWorldState.getInstance().setMapAtLocation(worldColumn, worldRow, map);
     }
 } 
--- tilemap.ts
+++ tilemap.ts
@@ -2,84 +2,18 @@ namespace SpriteKind {
     export const Decoration = SpriteKind.create()
 } 
 
-//% color=#84b89f
+//% color=#84b89f groups='["Operations","Values","Conversions","Directions"]'
 namespace tilemap {
     /**
-     * Returns the number of columns in the currently loaded tilemap.
+     * Determines if the tile in the loaded tilemap at the given location
+     * is of a particular kind.
      */
-    //% block="tilemap columns"
-    export function tilemapColumns(): number {
-        const tm = game.currentScene().tileMap;
-
-        if (!tm) return 0;
-        const height = tm.areaHeight() >> tm.scale;
-        return tm.areaWidth() >> tm.scale;
-    }
-
-    /**
-     * Returns the number of rows in the currently loaded tilemap.
-     */
-    //% block="tilemap rows"
-    export function tilemapRows(): number {
-        const tm = game.currentScene().tileMap;
-
-        if (!tm) return 0;
-        return tm.areaHeight() >> tm.scale;
-    }
-
-    /**
-     * Returns the width of tiles in the loaded tilemap.
-     */
-    //% block="tile width"
-    export function tileWidth(): number {
-        const tm = game.currentScene().tileMap;
-
-        if (!tm) return 0;
-        return 1 << tm.scale;
-    }
-
-    /**
-     * Replace all tiles of a given kind in the loaded tilemap with
-     * another tile.
-     */
-    //% block="replace all $from tiles with $to"
-    //% from.shadow=tileset_tile_picker
-    //% from.decompileIndirectFixedInstances=true
-    //% to.shadow=tileset_tile_picker
-    //% to.decompileIndirectFixedInstances=true
-    export function replaceAllTiles(from: Image, to: Image) {
-        forEachTileOfKind(from, function (col: number, row: number) {
-            tiles.setTileAt(tiles.getTileLocation(col, row), to);
-        });
-    }
-
-    /**
-     * Cover all tiles of a given kind in the loaded tilemap with
-     * another tile. Tiles are covered with sprites of kind Decoration.
-     */
-    //% block="cover all $tileKind tiles with $cover"
-    //% tileKind.shadow=tileset_tile_picker
-    //% tileKind.decompileIndirectFixedInstances=true
-    //% cover.shadow=tileset_tile_picker
-    //% cover.decompileIndirectFixedInstances=true
-    export function coverAllTiles(tileKind: Image, cover: Image) {
-        forEachTileOfKind(tileKind, function (col: number, row: number) {
-            coverTile(col, row, cover);
-        });
-    }
-
-    /**
-     * Cover the tile in the loaded tilemap at a given location with
-     * another tile. Tiles are covered with sprites of kind Decoration.
-     */
-    //% block="cover tile at col $col row $row with $cover"
-    //% cover.shadow=tileset_tile_picker
-    //% cover.decompileIndirectFixedInstances=true
-    export function coverTile(col: number, row: number, cover: Image) {
-        const coverSprite = sprites.create(cover, SpriteKind.Decoration);
-        coverSprite.setFlag(SpriteFlag.Ghost, true);
-        coverSprite.z = -1;
-        tiles.placeOnTile(coverSprite, tiles.getTileLocation(col, row));
+    //% block="tile at col $col row $row is $tile"
+    //% tile.shadow=tileset_tile_picker
+    //% tile.decompileIndirectFixedInstances=true
+    //% group="Operations" weight=80
+    export function tileIs(col: number, row: number, tile: Image): boolean {
+        return tile.equals(tiles.getTileAt(col, row));
     }
 
     /**
@@ -90,6 +24,7 @@ namespace tilemap {
     //% draggableParameters="reporter"
     //% tileKind.shadow=tileset_tile_picker
     //% tileKind.decompileIndirectFixedInstances=true
+    //% group="Operations" weight=70 blockGap=8
     export function forEachTileOfKind(tileKind: Image, cb: (col: number, row: number) => void) {
         const height = tilemapRows();
         const width = tilemapColumns();
@@ -106,6 +41,7 @@ namespace tilemap {
      */
     //% block="for each tile at $col $row with image $tile"
     //% draggableParameters="reporter"
+    //% group="Operations" weight=60
     export function forEachTileOfMap(cb: (col: number, row: number, tile: Image) => void) {
         const height = tilemapRows();
         const width = tilemapColumns();
@@ -118,14 +54,34 @@ namespace tilemap {
     }
 
     /**
-     * Determines if the tile in the loaded tilemap at the given location
-     * is of a particular kind.
+     * Cover the tile in the loaded tilemap at a given location with
+     * another tile. Tiles are covered with sprites of kind Decoration.
      */
-    //% block="tile at col $col row $row is $tile"
-    //% tile.shadow=tileset_tile_picker
-    //% tile.decompileIndirectFixedInstances=true
-    export function tileIs(col: number, row: number, tile: Image): boolean {
-        return tile.equals(tiles.getTileAt(col, row));
+    //% block="cover tile at col $col row $row with $cover"
+    //% cover.shadow=tileset_tile_picker
+    //% cover.decompileIndirectFixedInstances=true
+    //% group="Operations" weight=50 blockGap=8
+    export function coverTile(col: number, row: number, cover: Image) {
+        const coverSprite = sprites.create(cover, SpriteKind.Decoration);
+        coverSprite.setFlag(SpriteFlag.Ghost, true);
+        coverSprite.z = -1;
+        tiles.placeOnTile(coverSprite, tiles.getTileLocation(col, row));
+    }
+
+    /**
+     * Cover all tiles of a given kind in the loaded tilemap with
+     * another tile. Tiles are covered with sprites of kind Decoration.
+     */
+    //% block="cover all $tileKind tiles with $cover"
+    //% tileKind.shadow=tileset_tile_picker
+    //% tileKind.decompileIndirectFixedInstances=true
+    //% cover.shadow=tileset_tile_picker
+    //% cover.decompileIndirectFixedInstances=true
+    //% group="Operations" weight=40 blockGap=8
+    export function coverAllTiles(tileKind: Image, cover: Image) {
+        forEachTileOfKind(tileKind, function (col: number, row: number) {
+            coverTile(col, row, cover);
+        });
     }
     
     /**
@@ -134,7 +90,8 @@ namespace tilemap {
     //% block="on each $tileKind tile create a sprite of kind $spriteKind"
     //% tileKind.shadow=tileset_tile_picker
     //% tileKind.decompileIndirectFixedInstances=true
-    //% kind.shadow=spritekind
+    //% spriteKind.shadow=spritekind
+    //% group="Operations" weight=30 blockGap=8
     export function createSpritesOnTiles(tileKind: Image, spriteKind: number) {
         forEachTileOfKind(tileKind, (col, row) => {
             tiles.placeOnTile(sprites.create(img`.`), tiles.getTileLocation(col, row));
@@ -142,18 +99,73 @@ namespace tilemap {
     }
 
     /**
+     * Replace all tiles of a given kind in the loaded tilemap with
+     * another tile.
+     */
+    //% block="replace all $from tiles with $to"
+    //% from.shadow=tileset_tile_picker
+    //% from.decompileIndirectFixedInstances=true
+    //% to.shadow=tileset_tile_picker
+    //% to.decompileIndirectFixedInstances=true
+    //% group="Operations" weight=20
+    export function replaceAllTiles(from: Image, to: Image) {
+        forEachTileOfKind(from, function (col: number, row: number) {
+            tiles.setTileAt(tiles.getTileLocation(col, row), to);
+        });
+    }
+
+    /**
      * Center the camera on a given tile location.
      */
     //% block="center camera on col $col row $row"
+    //% group="Operations" weight=10 blockGap=8
     export function centerCameraOnTile(col: number, row: number) {
         const loc = tiles.getTileLocation(col, row);
         scene.centerCameraAt(loc.x, loc.y);
     }
 
     /**
+     * Returns the width of tiles in the loaded tilemap.
+     */
+    //% block="tile width"
+    //% group="Values" weight=30
+    export function tileWidth(): number {
+        const tm = game.currentScene().tileMap;
+
+        if (!tm) return 0;
+        return 1 << tm.scale;
+    }
+
+    /**
+     * Returns the number of columns in the currently loaded tilemap.
+     */
+    //% block="tilemap columns"
+    //% group="Values" weight=20 blockGap=8
+    export function tilemapColumns(): number {
+        const tm = game.currentScene().tileMap;
+
+        if (!tm) return 0;
+        const height = tm.areaHeight() >> tm.scale;
+        return tm.areaWidth() >> tm.scale;
+    }
+
+    /**
+     * Returns the number of rows in the currently loaded tilemap.
+     */
+    //% block="tilemap rows"
+    //% group="Values" weight=10 blockGap=8
+    export function tilemapRows(): number {
+        const tm = game.currentScene().tileMap;
+
+        if (!tm) return 0;
+        return tm.areaHeight() >> tm.scale;
+    }
+
+    /**
      * Converts a screen coordinate to a tilemap coordinate.
      */
-    //% block="screen $value to tile coordinate"
+    //% block="screen coordinate $value to tile coordinate"
+    //% group="Conversions" weight=30 blockGap=8
     export function screenCoordinateToTile(value: number) {
         const tm = game.currentScene().tileMap;
         if (!tm) return value >> 4;
@@ -163,7 +175,8 @@ namespace tilemap {
     /**
      * Converts a tilemap coordinate to a screen coordinate.
      */
-    //% block="tile $value to screen coordinate"
+    //% block="tile coordinate $value to screen coordinate"
+    //% group="Conversions" weight=20 blockGap=8
     export function tileCoordinateToScreen(value: number) {
         const tm = game.currentScene().tileMap;
         if (!tm) return value << 4;
@@ -174,7 +187,8 @@ namespace tilemap {
      * Converts a tilemap coordinate to a screen coordinate and
      * adds half a tile width.
      */
-    //% block="centered tile $value to screen coordinate"
+    //% block="centered tile coordinate $value to screen coordinate"
+    //% group="Conversions" weight=10 blockGap=8
     export function centeredTileCoordinateToScreen(value: number) {
         const tm = game.currentScene().tileMap;
         if (!tm) return (value << 4) + 8;
@@ -184,8 +198,10 @@ namespace tilemap {
     /**
      * Gets the neighboring column in the given direction.
      */
-    //% block="column $column in direction $direction"
-    export function columnInDirection(column: number, direction: WorldDirection) {
+    //% block="column $direction of $column"
+    //% direction.shadow=direction_editor
+    //% group="Directions" weight=40 blockGap=8
+    export function columnInDirection(column: number, direction: number) {
         if (direction === WorldDirection.East) return column + 1;
         else if (direction === WorldDirection.West) return column - 1;
         else return column;
@@ -194,8 +210,10 @@ namespace tilemap {
     /**
      * Gets the neighboring row in the given direction.
      */
-    //% block="row $row in direction $direction"
-    export function rowInDirection(row: number, direction: WorldDirection) {
+    //% block="row $direction of $row"
+    //% direction.shadow=direction_editor
+    //% group="Directions" weight=30
+    export function rowInDirection(row: number, direction: number) {
         if (direction === WorldDirection.North) return row - 1;
         else if (direction === WorldDirection.South) return row + 1;
         else return row;
@@ -207,10 +225,18 @@ namespace tilemap {
      */
     //% block="for each direction $direction"
     //% draggableParameters="reporter"
+    //% group="Directions" weight=20
     export function forEachDirection(cb: (direction: WorldDirection) => void) {
         cb(WorldDirection.North);
         cb(WorldDirection.East);
         cb(WorldDirection.South);
         cb(WorldDirection.West);
+    }
+
+    //% blockId=direction_editor shim=TD_ID
+    //% block="$direction"
+    //% group="Directions" weight=10
+    export function _directionEditor(direction: WorldDirection): WorldDirection {
+        return direction;
     }
 }
